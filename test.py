be4908a54f824548c808a4a52abfbf78302d58a8
Felix Reyero <felixreyero@gmail.com>
2020-10-14 21:52:48 -0300

    Initial project with test

---
+++ test.py
@@ -0,0 +1,12 @@
+from main import app
+import unittest
+
+
+class FlaskTest(unittest.TestCase):
+
+    # Check for response 200
+    def test_index(self):
+        tester = app.test_client(self)
+        response = tester.get("/")
+        statuscode = response.status_code
+        self.assertEqual(statuscode, 200)
