20f8d4a4b30a0cb3f41f8d596d2f8106c6e01c03
Felix Reyero <felixreyero@gmail.com>
2020-10-18 15:12:47 -0300

    Added port for heroku variable

--- main.py
+++ main.py
@@ -13,5 +13,7 @@ def health():
     return jsonify({"status": "UP"}), 200
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
